821f369e6a08d034b5dee7ef66eb729b6e473552
fechan <56131910+fechan@users.noreply.github.com>
2022-02-28 00:08:19 -0800

    make zipping script include top level folder

--- package_zip.sh
+++ package_zip.sh
@@ -1,2 +1,3 @@
 # Package the addon as a zip file, excluding the DataProcessing directory, hidden files/folders, and itself
-zip -r "JackJack.zip" . -x "./DataProcessing/*" ".*" "package_zip.sh"
+# run this in the parent folder of the JackJack repo!
+zip -r "JackJack/JackJack.zip" "JackJack/JackJack.toc" "JackJack/JackJack.lua" "JackJack/JackJackLocations.lua" "JackJack/fzy_lua.lua"
