43fcf3a49710506626b2cea464201729ac1ec5e7
fechan <56131910+fechan@users.noreply.github.com>
2022-03-13 01:05:46 -0800

    add dynamic searchbar

--- JackJack.lua
+++ JackJack.lua
@@ -13,62 +13,19 @@ JJ_SCROLLBAR_REGION_WIDTH = 30 -- not the actual width of the scrollbar, just th
 JJ_BUTTON_WIDTH = JJ_WIDTH - JJ_SCROLLBAR_REGION_WIDTH - JJ_MARGIN
 JJ_BUTTON_HEIGHT = 40
 JJ_CLOSE_BUTTON_HEIGHT = 40
+JJ_SEARCH_HEIGHT = 20
+JJ_SEARCH_WIDTH = JJ_WIDTH - (3 * JJ_MARGIN)
 
---- Sets up the main JackJack window frame
--- @return frame        UI frame for the window
--- @return scrollChild  UI frame for the location buttons container
-local function setUpFrame()
-    -- create the window
-    local frame = CreateFrame("Frame", "JackJackFrame", UIParent, "BackdropTemplate")
-    frame:SetSize(JJ_WIDTH, JJ_HEIGHT)
-    frame:SetPoint("CENTER")
-    frame:SetFrameStrata("HIGH")
-    frame:SetFrameLevel(1)
-    frame:Hide()
-
-    frame:SetBackdrop({
-        bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
-        edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
-        tile = true,
-        tileEdge = true,
-        tileSize = 8,
-        edgeSize = 8,
-        insets = { left = 1, right = 1, top = 1, bottom = 1 },
-    })
-
-    -- make it movable
-    frame:SetMovable(true)
-    frame:EnableMouse(true)
-    frame:RegisterForDrag("LeftButton")
-    frame:SetScript("OnDragStart", frame.StartMoving)
-    frame:SetScript("OnDragStop", frame.StopMovingOrSizing)
-
-    -- add a scroll frame which will contain location buttons
-    local scrollFrame = CreateFrame("ScrollFrame", "JackJackScrollFrame", frame, "UIPanelScrollFrameTemplate")
-    scrollFrame:SetPoint("TOPLEFT", frame, "TOPLEFT", JJ_MARGIN, -JJ_MARGIN)
-    scrollFrame:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", -JJ_SCROLLBAR_REGION_WIDTH, JJ_MARGIN * 2 + JJ_CLOSE_BUTTON_HEIGHT)
-
-    local scrollChild = CreateFrame("Frame")
-    scrollFrame:SetScrollChild(scrollChild)
-    scrollChild:SetWidth(1) -- not sure if setting this to 1 has any effect vs setting it to the parent's width
-    scrollChild:SetHeight(1) -- this can be any value, it doesn't matter
-
-    -- add big close button on bottom
-    local closeButton = CreateFrame("Button", "JackJackCloseButton", frame, "UIPanelButtonTemplate")
-    closeButton:SetSize(JJ_WIDTH - JJ_MARGIN, JJ_CLOSE_BUTTON_HEIGHT)
-    closeButton:SetPoint("BOTTOM", frame, "BOTTOM", 0, JJ_MARGIN)
-    closeButton:SetText("Cancel")
-    closeButton:SetScript("OnClick", function()
-        frame:Hide()
-    end)
-
-    return frame, scrollChild
-end
-
---- Sets up the location tooltip frame
-local function setUpLocationTooltip()
-    local tooltip = CreateFrame("GameTooltip", "JackJackLocationTooltip", UIParent, "GameTooltipTemplate")
-    return tooltip
+--- Set the text that displays the number of search results accordingly
+-- @param numResults the number of search results
+local function setSearchResultsText(numResults)
+    if numResults == 0 then
+        JJ_SEARCH_RESULTS_TXT:SetText("No possible matching locations found!")
+    elseif numResults < 20 then
+        JJ_SEARCH_RESULTS_TXT:SetText(numResults .. " possible matches")
+    else
+        JJ_SEARCH_RESULTS_TXT:SetText("Showing first 20 of " .. numResults .. " possible matches")
+    end
 end
 
 --- Get the display name for a location
@@ -146,39 +103,119 @@ local function hideLocationButtons()
     end
 end
 
-JJ_WINDOW, JJ_LOCATION_BUTTON_CONTAINER = setUpFrame()
-JJ_LOCATION_BUTTONS = {}
-JJ_TOOLTIP = setUpLocationTooltip()
-
-SlashCmdList["JACKJACK"] = function(msg, editBox)
-    local locationName = msg
-
+--- Sets the main JJ window to show matches for the given location name
+-- @param locationName Search string to match
+local function setLocationButtons(locationName)
     -- get all POIs that (fuzzy) match the location name
     local poiMatches = {}
     for rowNumber, poi in pairs(addon.locations) do
         if addon.fzy.has_match(locationName, poi["Name_lang"]) then
+            -- it's faster to precompute score so table.sort doesn't constantly recompute it in comparisons
+            poi["fzyScore"] = addon.fzy.score(locationName, poi["Name_lang"])
             table.insert(poiMatches, poi)
         end
     end
     -- sort by how close the match is to the location name
     table.sort(poiMatches, function(a, b)
-        return addon.fzy.score(locationName, a["Name_lang"]) > addon.fzy.score(locationName, b["Name_lang"])
+        return a["fzyScore"] > b["fzyScore"]
     end)
 
     -- update UI
-    if #poiMatches ~= 0 then
-        hideLocationButtons()
-        for buttonNumber, poi in ipairs(poiMatches) do
-            if not JJ_LOCATION_BUTTONS[buttonNumber] then
-                JJ_LOCATION_BUTTONS[buttonNumber] = addLocationButton(poi, buttonNumber)
-            else
-                local button = JJ_LOCATION_BUTTONS[buttonNumber]
-                modifyLocationButton(button, poi)
-                button:Show()
-            end
+    hideLocationButtons()
+    setSearchResultsText(#poiMatches)
+    for buttonNumber, poi in ipairs(poiMatches) do
+        if buttonNumber > 20 then break end
+        if not JJ_LOCATION_BUTTONS[buttonNumber] then
+            JJ_LOCATION_BUTTONS[buttonNumber] = addLocationButton(poi, buttonNumber)
+        else
+            local button = JJ_LOCATION_BUTTONS[buttonNumber]
+            modifyLocationButton(button, poi)
+            button:Show()
         end
-        JJ_WINDOW:Show()
-    else
-        print("No locations found for " .. locationName)
     end
+end
+
+--- Sets up the main JackJack window frame
+-- @return frame                UI frame for the window
+-- @return scrollChild          ScrollChild frame for the location buttons container
+-- @return searchBox            EditBox frame for the search box
+-- @return searchResultsText    FontString showing number of search resutls
+local function setUpFrame()
+    -- create the window
+    local frame = CreateFrame("Frame", "JackJackFrame", UIParent, "BackdropTemplate")
+    frame:SetSize(JJ_WIDTH, JJ_HEIGHT)
+    frame:SetPoint("CENTER")
+    frame:SetFrameStrata("HIGH")
+    frame:SetFrameLevel(1)
+    frame:Hide()
+
+    frame:SetBackdrop({
+        bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
+        edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
+        tile = true,
+        tileEdge = true,
+        tileSize = 8,
+        edgeSize = 8,
+        insets = { left = 1, right = 1, top = 1, bottom = 1 },
+    })
+
+    -- make it movable
+    frame:SetMovable(true)
+    frame:EnableMouse(true)
+    frame:RegisterForDrag("LeftButton")
+    frame:SetScript("OnDragStart", frame.StartMoving)
+    frame:SetScript("OnDragStop", frame.StopMovingOrSizing)
+
+    -- add a search box
+    local searchBox = CreateFrame("EditBox", "JackJackSearchBox", frame, "InputBoxTemplate")
+    searchBox:SetFontObject("GameFontNormal")
+    searchBox:SetSize(JJ_SEARCH_WIDTH, JJ_SEARCH_HEIGHT)
+    searchBox:SetPoint("TOPLEFT", frame, "TOPLEFT", JJ_MARGIN * 2, -JJ_MARGIN)
+    searchBox:SetAutoFocus(false)
+    searchBox:SetScript("OnTextChanged", function(self)
+        setLocationButtons(self:GetText())
+    end)
+
+    -- add text below search box that shows the number of locations found
+    local searchResultsText = searchBox:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    searchResultsText:SetPoint("BOTTOMLEFT", searchBox, "BOTTOMLEFT", 0, -2 * JJ_MARGIN)
+    searchResultsText:SetText("No possible matching locations found!")
+
+    -- add a scroll frame which will contain location buttons
+    local scrollFrame = CreateFrame("ScrollFrame", "JackJackScrollFrame", frame, "UIPanelScrollFrameTemplate")
+    scrollFrame:SetPoint("TOPLEFT", frame, "TOPLEFT", JJ_MARGIN, -4 * JJ_MARGIN - JJ_SEARCH_HEIGHT)
+    scrollFrame:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", -JJ_SCROLLBAR_REGION_WIDTH, JJ_MARGIN * 2 + JJ_CLOSE_BUTTON_HEIGHT)
+
+    local scrollChild = CreateFrame("Frame")
+    scrollFrame:SetScrollChild(scrollChild)
+    scrollChild:SetWidth(1) -- not sure if setting this to 1 has any effect vs setting it to the parent's width
+    scrollChild:SetHeight(1) -- this can be any value, it doesn't matter
+
+    -- add big close button on bottom
+    local closeButton = CreateFrame("Button", "JackJackCloseButton", frame, "UIPanelButtonTemplate")
+    closeButton:SetSize(JJ_WIDTH - JJ_MARGIN, JJ_CLOSE_BUTTON_HEIGHT)
+    closeButton:SetPoint("BOTTOM", frame, "BOTTOM", 0, JJ_MARGIN)
+    closeButton:SetText("Cancel")
+    closeButton:SetScript("OnClick", function()
+        frame:Hide()
+    end)
+
+    return frame, scrollChild, searchBox, searchResultsText
+end
+
+--- Sets up the location tooltip frame
+local function setUpLocationTooltip()
+    local tooltip = CreateFrame("GameTooltip", "JackJackLocationTooltip", UIParent, "GameTooltipTemplate")
+    return tooltip
+end
+
+JJ_WINDOW, JJ_LOCATION_BUTTON_CONTAINER, JJ_SEARCH_BOX, JJ_SEARCH_RESULTS_TXT = setUpFrame()
+JJ_LOCATION_BUTTONS = {}
+JJ_TOOLTIP = setUpLocationTooltip()
+
+SlashCmdList["JACKJACK"] = function(msg, editBox)
+    local locationName = msg
+    setLocationButtons(locationName)
+    JJ_SEARCH_BOX:SetText(locationName)
+    JJ_WINDOW:Show()
 end
