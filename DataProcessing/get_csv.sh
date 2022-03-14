15d996f367a6aa7e37ce1a5f33bb006a2b9c0873
fechan <56131910+fechan@users.noreply.github.com>
2022-03-14 00:51:31 -0700

    remove worldsafelocs from csv fetcher

--- DataProcessing/get_csv.sh
+++ DataProcessing/get_csv.sh
@@ -1,7 +1,7 @@
 # Get all the CSVs from WoW.tools with wget, because pandas.read_csv runs into HTTP 403 for some reason
-BUILD="9.2.0.42488"
+BUILD="9.2.0.42698"
 
-for TABLE in AreaPOI WorldSafeLocs WaypointNode WaypointSafeLocs AreaTable Map TaxiNodes
+for TABLE in AreaPOI WaypointNode WaypointSafeLocs AreaTable Map TaxiNodes
 do
 	wget "https://wow.tools/dbc/api/export/?name=${TABLE}&build=${BUILD}" -O "${TABLE}.csv"
 done
