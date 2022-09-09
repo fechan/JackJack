436da5088a8040b7bf95cf797b1d7075d76aad0f
fechan <56131910+fechan@users.noreply.github.com>
2022-09-09 15:15:02 -0700

    use SetUserData

--- GUIComponents/SearchPanel.lua
+++ GUIComponents/SearchPanel.lua
@@ -1,21 +1,6 @@
---- SearchPanel.lua: The panel for the location list with the search box above it
---- There should only ever be one of these at a time, so it's fine to keep state
---- as local variables here
-
 local addonName, addon = ...
 
 local AceGUI = LibStub("AceGUI-3.0")
-
-local resultsText
-
--- This is needed because locationListContainer doesn't fill
--- available height unless it's inside a container with the
--- "Fill" layout, even though SetFullHeight is set.
--- This also allows us to call ReleaseChildren on it to
--- delete the locationListContainer + locationList, since
--- we can't Release it directly without it breaking ReleaseChildren
--- for any parent elements.
-local locationListContainerContainer
 
 --- Get the text that displays the number of search results accordingly
 -- @param numResults    The number of search results
@@ -34,6 +19,7 @@ end
 
 local function populateLocationList(searchBox, callbackName, query)
     -- delete and create new location list
+    local locationListContainerContainer = searchBox:GetUserData("locationListContainerContainer")
     locationListContainerContainer:ReleaseChildren()
     local locationList, locationListContainer = addon.ScrollingList()
 
@@ -44,11 +30,13 @@ local function populateLocationList(searchBox, callbackName, query)
         locationList:AddChild(button)
     end
 
+    local resultsText = searchBox:GetUserData("resultsText")
     resultsText:SetText(getSearchResultsText(#locations, query == ""))
 
     locationListContainerContainer:AddChild(locationListContainer)
 end
 
+--- Factory method returning the panel for the location list with the search box above it
 function addon:SearchPanel()
     local searchPanel = AceGUI:Create("SimpleGroup")
     searchPanel:SetFullHeight(true)
@@ -66,13 +54,22 @@ function addon:SearchPanel()
     resultsText = AceGUI:Create("Label")
     resultsText:SetFullWidth(true)
     searchPanel:AddChild(resultsText)
+    searchBox:SetUserData("resultsText", resultsText)
 
-    -- container for the locationListContainer (see comment at variable declaration)
-    locationListContainerContainer = AceGUI:Create("SimpleGroup")
+    -- container for the locationListContainer
+    -- This is needed because locationListContainer doesn't fill
+    -- available height unless it's inside a container with the
+    -- "Fill" layout, even though SetFullHeight is set.
+    -- This also allows us to call ReleaseChildren on it to
+    -- delete the locationListContainer + locationList, since
+    -- we can't Release it directly without it breaking ReleaseChildren
+    -- for any parent elements.
+    local locationListContainerContainer = AceGUI:Create("SimpleGroup")
     locationListContainerContainer:SetFullHeight(true)
     locationListContainerContainer:SetFullWidth(true)
     locationListContainerContainer:SetLayout("Fill")
     searchPanel:AddChild(locationListContainerContainer)
+    searchBox:SetUserData("locationListContainerContainer", locationListContainerContainer)
 
     -- pre-populate the location list
     populateLocationList(searchBox, nil, "")
