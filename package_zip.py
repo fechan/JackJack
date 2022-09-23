67aa896faa54fa1e7e7051ba891c4ee2acdd8f5b
fechan <56131910+fechan@users.noreply.github.com>
2022-09-22 22:39:57 -0700

    update packager

--- package_zip.py
+++ package_zip.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
 from zipfile import ZipFile, ZIP_DEFLATED
+import os
 
+# get all files in TOC
 with open("JackJack.toc") as tocfile:
     toc = [line.strip().replace("\\", "/") for line in tocfile.readlines() if not (line.strip() == "" or line.startswith("#"))]
 
-with ZipFile("JackJack.zip", "w", ZIP_DEFLATED) as jjzip:
+# get version string
+with open("JackJack.toc") as tocfile:
+    version = [line.strip() for line in tocfile.readlines() if line.startswith("## Version:")][0]
+    version = version.replace("## Version: ", "")
+
+with ZipFile(os.path.join("Releases", version + ".zip"), "w", ZIP_DEFLATED) as jjzip:
     for file in toc:
         jjzip.write(file)
