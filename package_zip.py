59da9664244e3371d99e0b071fbd0a4a221b02d0
fechan <56131910+fechan@users.noreply.github.com>
2022-09-22 23:05:44 -0700

    add entire libs folder to archive

--- package_zip.py
+++ package_zip.py
@@ -5,13 +5,28 @@ import os
 
 # get all files in TOC
 with open("JackJack.toc") as tocfile:
-    toc = [line.strip().replace("\\", "/") for line in tocfile.readlines() if not (line.strip() == "" or line.startswith("#"))]
+    toc = [line.strip().replace("\\", "/") for line in tocfile.readlines() if not (line.strip() == "" or line.startswith("#") or line.startswith("Libs"))]
 
 # get version string
 with open("JackJack.toc") as tocfile:
     version = [line.strip() for line in tocfile.readlines() if line.startswith("## Version:")][0]
     version = version.replace("## Version: ", "")
 
+def zipdir(path, ziph):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file), 
+                       arcname=os.path.join(
+                            "JackJack",
+                            os.path.relpath(
+                                os.path.join(root, file), 
+                                os.path.join(path, '..')
+                            )
+                        )
+                    )
+
 with ZipFile(os.path.join("Releases", version + ".zip"), "w", ZIP_DEFLATED) as jjzip:
     for file in toc:
         jjzip.write(file, arcname=os.path.join("JackJack", file))
+    jjzip.write("JackJack.toc", arcname=os.path.join("JackJack", "JackJack.toc"))
+    zipdir("Libs", jjzip)
