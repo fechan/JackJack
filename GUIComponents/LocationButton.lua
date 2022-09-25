92d86e2ce9688d85d32b05f85f876e58913eac9c
fechan <56131910+fechan@users.noreply.github.com>
2022-09-25 14:16:12 -0700

    move display name generation to util

--- GUIComponents/LocationButton.lua
+++ GUIComponents/LocationButton.lua
@@ -7,33 +7,6 @@ local DEFAULT_STYLE = {
     ["FULLWIDTH"] = true
 }
 
---- Get the display name for a location
--- @param   location    Location to get display name for
--- @return  displayName Display name
-local function getLocationDisplayName(location)
-    if location.Transport ~= nil then
-        if location.Transport == "taxinode-geton" then
-            return location.DirectionNbr .. ". Take the flight master from " .. location.Name_lang
-        elseif location.Transport == "taxinode" then
-            return location.DirectionNbr .. ". Keep riding through " .. location.Name_lang
-        elseif location.Transport == "taxinode-getoff" then
-            return location.DirectionNbr .. ". Get off the flight master at " .. location.Name_lang
-        elseif location.Transport == "destination" then
-            return location.DirectionNbr .. ". Walk/fly to arrive at ".. location.Name_lang
-        else
-            return location.DirectionNbr .. ". " .. location.Name_lang
-        end
-    else
-        if location.AreaName_lang == "" or location.AreaName_lang == nil then
-            return location.Name_lang .. "\n(" .. location.MapName_lang .. ")"
-        elseif location.AreaName_lang == location.MapName_lang then
-            return location.Name_lang .. "\n(" .. location.MapName_lang .. ")"
-        else
-            return location.Name_lang .. "\n(" .. location.AreaName_lang .. ", " .. location.MapName_lang .. ")"
-        end
-    end
-end
-
 local function addTooltip(button, location, style)
     -- create tooltip
     local tooltip = addon:getGlobalTooltip()
@@ -42,7 +15,7 @@ local function addTooltip(button, location, style)
     tooltip:SetOwner(button.frame, "ANCHOR_RIGHT", 0, -style.HEIGHT)
 
     -- set text content
-    tooltip:AddLine(getLocationDisplayName(location))
+    tooltip:AddLine(addon.getLocationDisplayName(location))
     if location.Description_lang ~= "" and location.Description_lang ~= nil then
         tooltip:AddLine("Description: " .. location.Description_lang)
     end
@@ -77,7 +50,7 @@ function addon:LocationButton(location, style)
         button:GetUserData("tooltip"):Release()
     end)
 
-    button:SetText(getLocationDisplayName(location))
+    button:SetText(addon.getLocationDisplayName(location))
     button:SetHeight(style.HEIGHT)
     if style.FULLWIDTH then
         button:SetFullWidth(style.FULLWIDTH)
--- JackJackUtil.lua
+++ JackJackUtil.lua
@@ -1,5 +1,13 @@
 local addonName, addon = ...
 
+function table.slice(tbl, first, last, step)
+    local sliced = {}
+    for i = first or 1, last or #tbl, step or 1 do
+      sliced[#sliced+1] = tbl[i]
+    end
+    return sliced
+end
+
 --- Get a dataset safe ID for a record in a dataset that uniquely identifies it across all datasets
 -- @param datasetName   Unique name of the dataset
 -- @param recordId      ID of the record within the dataset
@@ -34,10 +42,26 @@ addon.getInstanceTypeName = function(instanceType)
     return ({[0] = "Not instanced", "Party dungeon", "Raid dungeon", "PvP battlefield", "Arena battlefield", "Scenario"})[tonumber(instanceType)]
 end
 
-function table.slice(tbl, first, last, step)
-    local sliced = {}
-    for i = first or 1, last or #tbl, step or 1 do
-      sliced[#sliced+1] = tbl[i]
+addon.getLocationDisplayName = function(location)
+    if location.Transport then -- if Transport is defined then this is a direction
+        if location.Transport == "taxinode-geton" then
+            return location.DirectionNbr .. ". Take the flight master from " .. location.Name_lang
+        elseif location.Transport == "taxinode" then
+            return location.DirectionNbr .. ". Keep riding through " .. location.Name_lang
+        elseif location.Transport == "taxinode-getoff" then
+            return location.DirectionNbr .. ". Get off the flight master at " .. location.Name_lang
+        elseif location.Transport == "destination" then
+            return location.DirectionNbr .. ". Walk/fly to arrive at ".. location.Name_lang
+        else
+            return location.DirectionNbr .. ". " .. location.Name_lang
+        end
+    else
+        if location.AreaName_lang == "" or location.AreaName_lang == nil then
+            return location.Name_lang .. "\n(" .. location.MapName_lang .. ")"
+        elseif location.AreaName_lang == location.MapName_lang then
+            return location.Name_lang .. "\n(" .. location.MapName_lang .. ")"
+        else
+            return location.Name_lang .. "\n(" .. location.AreaName_lang .. ", " .. location.MapName_lang .. ")"
+        end
     end
-    return sliced
 end
