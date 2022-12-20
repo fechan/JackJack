bbdb02aa6fc767c1710d2b2dfa7198eda525d30c
fechan <56131910+fechan@users.noreply.github.com>
2022-12-19 21:10:10 -0800

    add mage portal data

---
+++ Data/JJMagePortals.lua
@@ -0,0 +1,29 @@
+local addonName, addon = ...
+
+addon.JJMagePortals = {
+	[0]={["Name_lang_from"]=222,["PlayerConditionID"]="Create a portal to Stormwind",["Pos0"]=81254,["Pos1"]=-9041.0,["MapID"]=917.66},
+	[1]={["Name_lang_from"]=223,["PlayerConditionID"]="Create a portal to Ironforge",["Pos0"]=81255,["Pos1"]=-4613.71,["MapID"]=-915.29},
+	[2]={["Name_lang_from"]=224,["PlayerConditionID"]="Create a portal to Darnassus",["Pos0"]=81256,["Pos1"]=7415.11,["MapID"]=46.24},
+	[3]={["Name_lang_from"]=225,["PlayerConditionID"]="Create a portal to Exodar",["Pos0"]=81257,["Pos1"]=-4031.24,["MapID"]=-11569.6},
+	[4]={["Name_lang_from"]=226,["PlayerConditionID"]="Create a portal to Theramore",["Pos0"]=81258,["Pos1"]=-3748.11,["MapID"]=-4440.21},
+	[5]={["Name_lang_from"]=227,["PlayerConditionID"]="Create a portal to Orgrimmar",["Pos0"]=81259,["Pos1"]=1445.21,["MapID"]=-4499.56},
+	[6]={["Name_lang_from"]=228,["PlayerConditionID"]="Create a portal to Undercity",["Pos0"]=81260,["Pos1"]=1947.26,["MapID"]=-98.15},
+	[7]={["Name_lang_from"]=229,["PlayerConditionID"]="Create a portal to Thunder Bluff",["Pos0"]=81261,["Pos1"]=-967.38,["MapID"]=284.82},
+	[8]={["Name_lang_from"]=230,["PlayerConditionID"]="Create a portal to Silvermoon",["Pos0"]=81262,["Pos1"]=9998.46,["MapID"]=-7106.55},
+	[9]={["Name_lang_from"]=231,["PlayerConditionID"]="Create a portal to Stonard",["Pos0"]=81263,["Pos1"]=-10469.0,["MapID"]=-3331.54},
+	[10]={["Name_lang_from"]=232,["PlayerConditionID"]="Create a portal to Shattrath",["Pos0"]=81264,["Pos1"]=-1824.32,["MapID"]=5417.23},
+	[11]={["Name_lang_from"]=233,["PlayerConditionID"]="Create a portal to Shattrath",["Pos0"]=81265,["Pos1"]=-1902.49,["MapID"]=5442.86},
+	[12]={["Name_lang_from"]=234,["PlayerConditionID"]="Create a portal to Dalaran - Northrend",["Pos0"]=81266,["Pos1"]=5807.75,["MapID"]=588.35},
+	[13]={["Name_lang_from"]=235,["PlayerConditionID"]="Create a portal to Tol Barad",["Pos0"]=81267,["Pos1"]=-369.21,["MapID"]=1058.73},
+	[14]={["Name_lang_from"]=236,["PlayerConditionID"]="Create a portal to Tol Barad",["Pos0"]=81268,["Pos1"]=-603.72,["MapID"]=1387.62},
+	[15]={["Name_lang_from"]=237,["PlayerConditionID"]="Create a portal to Dalaran - Ancient",["Pos0"]=81269,["Pos1"]=303.22,["MapID"]=347.21},
+	[16]={["Name_lang_from"]=238,["PlayerConditionID"]="Create a portal to Vale of Eternal Blossoms",["Pos0"]=81270,["Pos1"]=917.64,["MapID"]=294.88},
+	[17]={["Name_lang_from"]=239,["PlayerConditionID"]="Create a portal to Vale of Eternal Blossoms",["Pos0"]=81271,["Pos1"]=1579.65,["MapID"]=897.84},
+	[18]={["Name_lang_from"]=240,["PlayerConditionID"]="Create a portal to Stormshield",["Pos0"]=81272,["Pos1"]=3744.33,["MapID"]=-4055.88},
+	[19]={["Name_lang_from"]=241,["PlayerConditionID"]="Create a portal to Warspear",["Pos0"]=81273,["Pos1"]=5267.77,["MapID"]=-4060.26},
+	[20]={["Name_lang_from"]=242,["PlayerConditionID"]="Create a portal to Hall of the Guardian",["Pos0"]=81275,["Pos1"]=-935.17,["MapID"]=4706.4},
+	[21]={["Name_lang_from"]=243,["PlayerConditionID"]="Create a portal to Dalaran - Broken Isles",["Pos0"]=81276,["Pos1"]=-828.72,["MapID"]=4371.78},
+	[22]={["Name_lang_from"]=244,["PlayerConditionID"]="Create a portal to Boralus",["Pos0"]=81277,["Pos1"]=1137.36,["MapID"]=-538.6},
+	[23]={["Name_lang_from"]=245,["PlayerConditionID"]="Create a portal to Dazar'alor",["Pos0"]=81278,["Pos1"]=-1129.19,["MapID"]=774.15},
+	[24]={["Name_lang_from"]=287,["PlayerConditionID"]="Create a portal to Oribos",["Pos0"]=88449,["Pos1"]=-1834.21,["MapID"]=1542.27}
+}
